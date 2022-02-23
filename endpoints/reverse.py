eaeb94b8d98a7bd039cef7938e8cbc2f9b0e69cf
k4is3r <ejwimery@gmail.com>
2022-02-23 16:01:19 -0600

    creating endpoint for reverse

---
+++ endpoints/reverse.py
@@ -0,0 +1,25 @@
+from fastapi import APIRouter
+from api_functions import ResponseMode, ErrorResponseModel
+import geocoder
+
+router = APIRouter()
+
+
+@router.get('/reverse',
+        name="Reverse Geocoding",
+        description="Returns the address of given geographical coordinates")
+async def reverse(lat: float, lon: float):
+    """Returns the address of a given geographical coordinates"""
+    try:
+        geolocator = geocoder.osm([lat, lon], method='reverse')
+        address = geolocator.json['raw']['display_name']
+        data = {
+            "source": "OSM",
+            "longitude": lon,
+            "latitude": lat,
+            "address": address
+        }
+        return ResponseModel(data, "success")
+    except Exception:
+        return ErrorResponseModel(503, "Internal Server Error")
+
